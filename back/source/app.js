b5633b1a97abf65cc9f8f414a656d27cbdced2ef
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-05-15 18:29:42 -0500

    Back add cors

--- back/source/app.js
+++ back/source/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors())
 app.use(helmet());
 app.use(bodyParser.json());
 
