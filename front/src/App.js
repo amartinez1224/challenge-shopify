8267aa6db81cfd4cbeb9cea6e97966f0e0e666df
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-05-15 18:47:17 -0500

    Fetch warehouses and items

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const url = "https://challenge-shopify.amartinez1224.repl.co"
+  const itemsAddress = "/items"
+  const warehousesAddress = "/warehouses"
+
+  let [items, setItems] = useState([]);
+  let [warehouses, setWarehouses] = useState([]);
+
+  useEffect(() => {
+    fetchItems()
+    fetchWarehouses()
+  },[]);
+
+  function fetchItems() {
+    fetch(url + itemsAddress).then(response => {
+      response.json()
+        .then(items => {
+          setItems(items)
+        })
+    })
+  }
+
+  function fetchWarehouses() {
+    fetch(url + warehousesAddress).then(response => {
+      response.json()
+        .then(warehouses => {
+          setWarehouses(warehouses)
+        })
+    })
+  }
+
   return (
     <div className="container">
-      <div class="row">
+      <div className="row">
         <h1>Shopify Challenge</h1>
-        <br/>
+        <br />
       </div>
-      <div class="row">
-        <div class="col-8">
+      <div className="row">
+        <div className="col-8">
           <h3>Items</h3>
         </div>
-        <div class="col-4">
+        <div className="col-4">
           <h3>Warehouses</h3>
         </div>
       </div>
