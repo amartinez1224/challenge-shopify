5dca42f6f19262abf71489f8e8e10de6867d72eb
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-05-15 09:05:25 -0500

    Server listening

---
+++ back/source/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+
+function notFound(req, res, next) {
+    res.status(404);
+    const error = new Error('not found', req.originalUrl);
+    next(error);
+}
+
+function errorHandler(err, req, res, next){
+    res.status(res.statusCode || 500);
+    res.json({
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+// errors
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
