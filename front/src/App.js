d6b761a0de83f5ce5fdecb3435af9a6b96f66f79
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-05-15 19:20:37 -0500

    Add warehouse and item forms

--- front/src/App.js
+++ front/src/App.js
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     fetchItems()
     fetchWarehouses()
-  },[]);
+  }, []);
 
   function fetchItems() {
     fetch(url + itemsAddress).then(response => {
@@ -35,16 +35,30 @@ function App() {
     })
   }
 
-  function showItems(){
-    return items.map((item,i) => {
-      return <Item key={i} item={item}/>
+  function showItems() {
+    return items.map((item, i) => {
+      return <Item key={i} item={item} />
     })
   }
 
-  function showWarehouses(){
-    return warehouses.map((warehouse,i) => {
-      return <Warehouse key={i} warehouse={warehouse}/>
+  function showWarehouses() {
+    return warehouses.map((warehouse, i) => {
+      return <Warehouse key={i} warehouse={warehouse} />
     })
+  }
+
+  function addWarehouse(e){
+    e.preventDefault()
+    console.log(e.target[0].value)
+    console.log(e.target[1].value)
+    e.target.reset()
+  }
+
+  function addItem(e){
+    e.preventDefault()
+    console.log(e.target[0].value)
+    console.log(e.target[1].value)
+    e.target.reset()
   }
 
   return (
@@ -54,17 +68,43 @@ function App() {
         <br />
       </div>
       <div className="row">
-        <div className="col-8">
+        <div className="col-8 pe-5">
           <h3>Items</h3>
           {showItems()}
-          <br/>
-          <h5>Add Item</h5>
+          <br />
+          <h5>New Item</h5>
+
+          <form onSubmit={addItem}>
+            <div className="mb-3">
+              <label className="form-label">Name</label>
+              <input type="text" className="form-control"/>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Description</label>
+              <textarea className="form-control"/>
+            </div>
+            <button type="submit" className="btn btn-primary">Add item</button>
+          </form>
+
         </div>
         <div className="col-4">
           <h3>Warehouses</h3>
           {showWarehouses()}
-          <br/>
-          <h5>Add Warehouse</h5>
+          <br />
+
+          <h5>New Warehouse</h5>
+          <form onSubmit={addWarehouse}>
+            <div className="mb-3">
+              <label className="form-label">Name</label>
+              <input type="text" className="form-control"/>
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Address</label>
+              <input type="text" className="form-control"/>
+            </div>
+            <button type="submit" className="btn btn-primary">Add warehouse</button>
+          </form>
+
         </div>
       </div>
     </div>
