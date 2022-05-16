4996e714446e4d0f7d67288375d66873dae2cb69
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-05-16 11:39:32 -0500

    Single script to run

--- back/source/server.js
+++ back/source/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.listen(port,'127.0.0.1', () => {
   console.log(`listening on port ${port}`);
 });
