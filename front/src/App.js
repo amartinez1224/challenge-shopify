997f4490197a8b6a1daf62e123c6166c30b2bfb8
Andres Martinez <44838230+amartinez1224@users.noreply.github.com>
2022-05-15 20:04:12 -0500

    add warehouse to db and alert

--- front/src/App.js
+++ front/src/App.js
@@ -11,12 +11,23 @@ function App() {
 
   let [items, setItems] = useState([]);
   let [warehouses, setWarehouses] = useState([]);
+  let [alert, setAlert] = useState({ message: "Well done!", title: "Well done!", theme: "alert-success", show: false })
 
   useEffect(() => {
     fetchItems()
     fetchWarehouses()
   }, []);
 
+  // alert only for 2.5 sec
+  useEffect(() => {
+    if (alert.show){
+      setTimeout(() => {
+        setAlert({...alert,show: false})
+      }, 2500);
+    }
+  },[alert])
+
+  // fetch items
   function fetchItems() {
     fetch(url + itemsAddress).then(response => {
       response.json()
@@ -26,6 +37,7 @@ function App() {
     })
   }
 
+  // fetch warehouses
   function fetchWarehouses() {
     fetch(url + warehousesAddress).then(response => {
       response.json()
@@ -35,30 +47,61 @@ function App() {
     })
   }
 
+  // show items
   function showItems() {
     return items.map((item, i) => {
       return <Item key={i} item={item} />
     })
   }
 
+  // show warehouses
   function showWarehouses() {
     return warehouses.map((warehouse, i) => {
       return <Warehouse key={i} warehouse={warehouse} />
     })
   }
 
-  function addWarehouse(e){
+  // add warehouse
+  function addWarehouse(e) {
+    e.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: e.target[0].value, address: e.target[1].value })
+    };
+    fetch(url + warehousesAddress, requestOptions)
+      .then(response => {
+        if (response.status == 201) {
+          setAlert({message: "Warehouse added", title: "Succes", theme: "alert-success", show: true});
+        }
+        else {
+          response.json().then(data => {
+            setAlert ({message: data.message, title: "Failed", theme: "alert-danger", show: true});
+          });
+        }
+        fetchWarehouses();
+      })
+    e.target.reset();
+  }
+
+  // add item
+  function addItem(e) {
     e.preventDefault()
     console.log(e.target[0].value)
     console.log(e.target[1].value)
     e.target.reset()
   }
 
-  function addItem(e){
-    e.preventDefault()
-    console.log(e.target[0].value)
-    console.log(e.target[1].value)
-    e.target.reset()
+  // show alert
+  function showAlert() {
+    if (alert.show) {
+      return (
+        <div className={"alert alert-fixed " + alert.theme} role="alert">
+          <h4 className="alert-heading">{alert.title}</h4>
+          <p>{alert.message}</p>
+        </div>
+      )
+    }
   }
 
   return (
@@ -77,11 +120,11 @@ function App() {
           <form onSubmit={addItem}>
             <div className="mb-3">
               <label className="form-label">Name</label>
-              <input type="text" className="form-control"/>
+              <input type="text" className="form-control" />
             </div>
             <div className="mb-3">
               <label className="form-label">Description</label>
-              <textarea className="form-control"/>
+              <textarea className="form-control" />
             </div>
             <button type="submit" className="btn btn-primary">Add item</button>
           </form>
@@ -96,17 +139,18 @@ function App() {
           <form onSubmit={addWarehouse}>
             <div className="mb-3">
               <label className="form-label">Name</label>
-              <input type="text" className="form-control"/>
+              <input type="text" className="form-control" />
             </div>
             <div className="mb-3">
               <label className="form-label">Address</label>
-              <input type="text" className="form-control"/>
+              <input type="text" className="form-control" />
             </div>
             <button type="submit" className="btn btn-primary">Add warehouse</button>
           </form>
 
         </div>
       </div>
+      {showAlert()}
     </div>
   );
 }
